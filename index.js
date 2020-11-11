866cf8849d0087eec2cfc8490195f2b6e08b420f
EliPreston <eli.preston@ucc.on.ca>
2020-11-11 12:57:55 -0500

    worked on opening

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    // $(".fixed-action-btn").on("click", function () {
+    //   window.scroll(0, 0);
+    // });
     $(".sidenav").sidenav();
     $(".dropdown-trigger").dropdown();
     $(".materialboxed").materialbox();
@@ -9,4 +12,19 @@ $(document).ready(function () {
     });
     $(".tooltipped").tooltip();
     $(".scrollspy").scrollSpy();
+    
   });
+
+
+
+$(window).scroll(function() { // when the page is scrolled run this
+  if($(this).scrollTop() != 0) { // if you're NOT at the top
+      $('#top').fadeIn("slow"); // fade in
+  } else { // else
+      $('#top').fadeOut("slow"); // fade out
+  }
+});
+
+$('#top').click(function() { // when the button is clicked
+  $('body,html').animate({scrollTop:0},1000); // return to the top with a nice animation
+});
