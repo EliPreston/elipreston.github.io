92512868a998d99c4c5e77c1a7b4fc663aeeb689
EliPreston <eli.preston@ucc.on.ca>
2020-11-10 23:25:38 -0500

    github.io

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".sidenav").sidenav();
+    $(".dropdown-trigger").dropdown();
+    $(".materialboxed").materialbox();
+    $(".parallax").parallax();
+    $(".tabs").tabs();
+    $(".datepicker").datepicker({
+      disableWeekends: true
+    });
+    $(".tooltipped").tooltip();
+    $(".scrollspy").scrollSpy();
+  });
