55228aec62e4a93fd3b2281dd6ec59698acef123
EliPreston <eli.preston@ucc.on.ca>
2021-02-09 10:19:15 -0500

    update

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ $(document).ready(function () {
 
 
 
-$(window).scroll(function() { // when the page is scrolled run this
-  if($(this).scrollTop() != 0) { // if you're NOT at the top
-      $('#top').fadeIn("slow"); // fade in
-  } else { // else
-      $('#top').fadeOut("slow"); // fade out
-  }
-});
-
 $('#top').click(function() { // when the button is clicked
   $('body,html').animate({scrollTop:0},1000); // return to the top with animation
 });
+
+// $(window).scroll(function() { // when the page is scrolled run this
+//   if($(this).scrollTop() != 0) { // if you're NOT at the top
+//       $('#top').fadeIn("slow"); // fade in
+//   } else { // else
+//       $('#top').fadeOut("slow"); // fade out
+//   }
+// });
